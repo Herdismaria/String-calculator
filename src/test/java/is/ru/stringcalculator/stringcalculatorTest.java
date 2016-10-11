13db0513130904129a028a9da84e64decddb70b1
herdismaria <herdismaria90@gmail.com>
2016-10-11 09:38:35 +0200

    ChChanged some filenames and wrote the first tests for add function

--- src/test/java/is/ru/stringcalculator/stringcalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/stringcalculatorTest.java
@@ -3,10 +3,35 @@ package is.ru.stringcalculator;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test
 
-public class CalculatorTest
+public class StringCalculatorTest
 {
-	public static void main (string args[])
+	public static void main (String args[])
 	{
-	
+		org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
 	}
+		
+	@Test
+	public void TestAddEmptyString()
+	{
+		assertEquals(0, stringcalculator.add(""));
+	}
+
+	@Test
+	public void TestAddEmptyString()
+	{
+		assertEquals(1, stringcalculator.add("1"));
+	}
+
+	@Test
+	public void TestAddEmptyString()
+	{
+		assertEquals(3, stringcalculator.add("1,2"));
+	}
+
+	@Test
+	public void TestAddEmptyString()
+	{
+		assertEquals(-1, stringcalculator.add("xxx"));
+	}
+
 }
